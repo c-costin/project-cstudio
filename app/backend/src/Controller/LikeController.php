cd82ec6f00719cd3dabaaff74d9fb813f6529aa6
c-costin <contact@costincadeau.fr>
2023-08-31 23:03:15 +0200

    feat: document Like endpoints | Rework after create Like service

--- app/backend/src/Controller/LikeController.php
+++ app/backend/src/Controller/LikeController.php
@@ -2,16 +2,60 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use OpenApi\Attributes as OA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[OA\Tag('Like')]
 #[Route('/api/like')]
 class LikeController extends AbstractController
 {
     #[Route('/', name: 'app_like_browse', methods: 'GET')]
+    #[OA\Get(
+        summary: "Retrieve the collection of Like resources",
+        description: "Retrieve the collection of Like",
+    )]
+    #[OA\Parameter(
+        name: "user",
+        description: "User ID",
+        in: "query",
+        required: true
+    )]
+    #[OA\Response(
+        response: 200,
+        description: "Success",
+        content: new OA\JsonContent(
+            type: 'array',
+            items: new OA\Items(ref: new Model(type: Product::class, groups: ['read:Product:item']))
+        ),
+    )]
+    #[OA\Response(
+        response: 401,
+        description: "Unauthorized",
+        content: new OA\JsonContent(
+            type: "object",
+            properties: [
+                new OA\Property(property: "code", example: 401),
+                new OA\Property(property: "message", example: "Invalid credentials")
+            ]
+        )
+    )]
+    #[OA\Response(
+        response: 404,
+        description: "Not Found",
+        content: new OA\JsonContent(
+            type: "object",
+            properties: [
+                new OA\Property(property: "code", example: 404),
+                new OA\Property(property: "message", example: "No Likes was found")
+            ]
+        )
+    )]
     public function browse(Request $request,): JsonResponse
     {
         // Parameters into query
@@ -33,6 +77,59 @@ class LikeController extends AbstractController
     }
 
     #[Route('/add', name: 'app_like_add', methods: 'GET')]
+    #[OA\Post(
+        summary: "Create a Like resource",
+        description: "Create a Like resource",
+    )]
+    #[OA\Parameter(
+        name: "product",
+        description: "Product ID",
+        in: "query",
+        required: true
+    )]
+    #[OA\Parameter(
+        name: "user",
+        description: "User ID",
+        in: "query",
+        required: true
+    )]
+    #[OA\Response(
+        response: 204,
+        description: "Success - No Content",
+    )]
+    #[OA\Response(
+        response: 400,
+        description: "Bad request",
+        content: new OA\JsonContent(
+            type: "object",
+            properties: [
+                new OA\Property(property: "code", example: 400),
+                new OA\Property(property: "message", example: "Invalid request")
+            ]
+        )
+    )]
+    #[OA\Response(
+        response: 401,
+        description: "Unauthorized",
+        content: new OA\JsonContent(
+            type: "object",
+            properties: [
+                new OA\Property(property: "code", example: 401),
+                new OA\Property(property: "message", example: "Invalid credentials")
+            ]
+        )
+    )]
+    #[OA\Response(
+        response: 403,
+        description: "Forbidden",
+        content: new OA\JsonContent(
+            type: "object",
+            properties: [
+                new OA\Property(property: "code", example: 403),
+                new OA\Property(property: "message", example: "Access Denied")
+            ]
+        )
+    )]
     public function add(Request $request): JsonResponse
     {
         // Parameters into query
@@ -42,7 +139,7 @@ class LikeController extends AbstractController
                 $result = []; //TODO Make find method into like service
 
                 if ($result !== []) {
-                    return $this->json($result, Response::HTTP_OK, []); //TODO Find solution for serialization
+                    return $this->json(null, Response::HTTP_NO_CONTENT, []); //TODO Find solution for serialization
                 } else {
                     return $this->json(["code" => 404, "message" => "No Like was found"], Response::HTTP_NOT_FOUND);
                 }
@@ -54,6 +151,47 @@ class LikeController extends AbstractController
     }
 
     #[Route('/delete', name: 'app_like_delete', methods: 'GET')]
+    #[OA\Delete(
+        summary: "Remove a Product resource",
+        description: "Remove a Product resource",
+    )]
+    #[OA\Response(
+        response: 204,
+        description: "Success - No Content",
+    )]
+    #[OA\Response(
+        response: 401,
+        description: "Unauthorized",
+        content: new OA\JsonContent(
+            type: "object",
+            properties: [
+                new OA\Property(property: "code", example: 401),
+                new OA\Property(property: "message", example: "Invalid credentials")
+            ]
+        )
+    )]
+    #[OA\Response(
+        response: 403,
+        description: "Forbidden",
+        content: new OA\JsonContent(
+            type: "object",
+            properties: [
+                new OA\Property(property: "code", example: 403),
+                new OA\Property(property: "message", example: "Access Denied")
+            ]
+        )
+    )]
+    #[OA\Response(
+        response: 404,
+        description: "Not Found",
+        content: new OA\JsonContent(
+            type: "object",
+            properties: [
+                new OA\Property(property: "code", example: 404),
+                new OA\Property(property: "message", example: "No Like was found")
+            ]
+        )
+    )]
     public function delete(Request $request): JsonResponse
     {
         // Parameters into query
