7b809106237a7ff44b41ab1b810a24e714eaab33
c-costin <contact@costincadeau.fr>
2023-08-31 22:30:10 +0200

    feat: document Type endpoints and create Type group for serialization

--- app/backend/src/Controller/TypeController.php
+++ app/backend/src/Controller/TypeController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Type;
 use App\Repository\TypeRepository;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use OpenApi\Attributes as OA;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,11 +14,46 @@ use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+#[OA\Tag('Type')]
 #[Route('/api/type')]
 class TypeController extends AbstractController
 {
     // List all the Types
     #[Route('/', name: 'app_type_browse', methods: 'GET')]
+    #[OA\Get(
+        summary: "Retrieve the collection of Type resources",
+        description: "Retrieve the collection of Type",
+    )]
+    #[OA\Response(
+        response: 200,
+        description: "Success",
+        content: new OA\JsonContent(
+            type: 'array',
+            items: new OA\Items(ref: new Model(type: Type::class, groups: ['read:Type:item']))
+        ),
+    )]
+    #[OA\Response(
+        response: 401,
+        description: "Unauthorized",
+        content: new OA\JsonContent(
+            type: "object",
+            properties: [
+                new OA\Property(property: "code", example: 401),
+                new OA\Property(property: "message", example: "Invalid credentials")
+            ]
+        )
+    )]
+    #[OA\Response(
+        response: 404,
+        description: "Not Found",
+        content: new OA\JsonContent(
+            type: "object",
+            properties: [
+                new OA\Property(property: "code", example: 404),
+                new OA\Property(property: "message", example: "No Products was found")
+            ]
+        )
+    )]
     public function browse(TypeRepository $typeRepository): JsonResponse
     {
         return $this->json($typeRepository->findAll(), Response::HTTP_OK, [], ["groups" => ["read:Type:item"]]);
@@ -24,6 +61,37 @@ class TypeController extends AbstractController
 
     // Show a Type
     #[Route('/{id<\d+>}', name: 'app_type_read', methods: ['GET'])]
+    #[OA\Get(
+        summary: "Retrieve a Type resource",
+        description: "Retrieve a Type resource",
+    )]
+    #[OA\Response(
+        response: 200,
+        description: "Success",
+        content: new Model(type: Type::class, groups: ['read:Type:item'])
+    )]
+    #[OA\Response(
+        response: 401,
+        description: "Unauthorized",
+        content: new OA\JsonContent(
+            type: "object",
+            properties: [
+                new OA\Property(property: "code", example: 401),
+                new OA\Property(property: "message", example: "Invalid credentials")
+            ]
+        )
+    )]
+    #[OA\Response(
+        response: 404,
+        description: "Not Found",
+        content: new OA\JsonContent(
+            type: "object",
+            properties: [
+                new OA\Property(property: "code", example: 404),
+                new OA\Property(property: "message", example: "No Type was found")
+            ]
+        )
+    )]
     public function read(Type $type = null): JsonResponse
     {
         // Return status code 404 if $Type is empty
@@ -37,6 +105,58 @@ class TypeController extends AbstractController
 
     // Update a Type
     #[Route('/edit/{id<\d+>}', name: 'app_type_edit', methods: ['PATCH'])]
+    #[OA\Patch(
+        summary: "Update a Type resource",
+        description: "Update a Type resource",
+        requestBody: new OA\RequestBody(
+            content: new OA\MediaType(
+                mediaType: "application/json",
+                schema: new OA\Schema(
+                    properties: [
+                        new OA\Property(property: "name", example: "Peinture"),
+                    ]
+                )
+            )
+        )
+    )]
+    #[OA\Response(
+        response: 202,
+        description: "Success - Accepted",
+        content: new Model(type: Type::class, groups: ['read:Type:item'])
+    )]
+    #[OA\Response(
+        response: 401,
+        description: "Unauthorized",
+        content: new OA\JsonContent(
+            type: "object",
+            properties: [
+                new OA\Property(property: "code", example: 401),
+                new OA\Property(property: "message", example: "Invalid credentials")
+            ]
+        )
+    )]
+    #[OA\Response(
+        response: 403,
+        description: "Forbidden",
+        content: new OA\JsonContent(
+            type: "object",
+            properties: [
+                new OA\Property(property: "code", example: 403),
+                new OA\Property(property: "message", example: "Access Denied")
+            ]
+        )
+    )]
+    #[OA\Response(
+        response: 404,
+        description: "Not Found",
+        content: new OA\JsonContent(
+            type: "object",
+            properties: [
+                new OA\Property(property: "code", example: 404),
+                new OA\Property(property: "message", example: "No Type was found")
+            ]
+        )
+    )]
     public function edit(Type $type = null, Request $request, SerializerInterface $serializerInterface, TypeRepository $TypeRepository): JsonResponse
     {
         if ($type === null) {
@@ -60,6 +180,58 @@ class TypeController extends AbstractController
 
     // Create a Type
     #[Route('/add', name: 'app_type_add', methods: ['POST'])]
+    #[OA\Post(
+        summary: "Create a Product resource",
+        description: "Create a Product resource",
+        requestBody: new OA\RequestBody(
+            content: new OA\MediaType(
+                mediaType: "application/json",
+                schema: new OA\Schema(
+                    properties: [
+                        new OA\Property(property: "name", example: "Peinture"),
+                    ]
+                )
+            )
+        )
+    )]
+    #[OA\Response(
+        response: 201,
+        description: "Success - Created",
+        content: new Model(type: Type::class, groups: ['read:Type:item'])
+    )]
+    #[OA\Response(
+        response: 400,
+        description: "Bad request",
+        content: new OA\JsonContent(
+            type: "object",
+            properties: [
+                new OA\Property(property: "code", example: 400),
+                new OA\Property(property: "message", example: "Invalid request")
+            ]
+        )
+    )]
+    #[OA\Response(
+        response: 401,
+        description: "Unauthorized",
+        content: new OA\JsonContent(
+            type: "object",
+            properties: [
+                new OA\Property(property: "code", example: 401),
+                new OA\Property(property: "message", example: "Invalid credentials")
+            ]
+        )
+    )]
+    #[OA\Response(
+        response: 403,
+        description: "Forbidden",
+        content: new OA\JsonContent(
+            type: "object",
+            properties: [
+                new OA\Property(property: "code", example: 403),
+                new OA\Property(property: "message", example: "Access Denied")
+            ]
+        )
+    )]
     public function add(Request $request, SerializerInterface $serializerInterface, TypeRepository $typeRepository): JsonResponse
     {
         // Get Request Body
@@ -82,6 +254,47 @@ class TypeController extends AbstractController
 
     // Remove a Type
     #[Route('/delete/{id<\d+>}', name: 'app_type_delete', methods: ['DELETE'])]
+    #[OA\Delete(
+        summary: "Remove a Type resource",
+        description: "Remove a Type resource",
+    )]
+    #[OA\Response(
+        response: 204,
+        description: "Success - No Content",
+    )]
+    #[OA\Response(
+        response: 401,
+        description: "Unauthorized",
+        content: new OA\JsonContent(
+            type: "object",
+            properties: [
+                new OA\Property(property: "code", example: 401),
+                new OA\Property(property: "message", example: "Invalid credentials")
+            ]
+        )
+    )]
+    #[OA\Response(
+        response: 403,
+        description: "Forbidden",
+        content: new OA\JsonContent(
+            type: "object",
+            properties: [
+                new OA\Property(property: "code", example: 403),
+                new OA\Property(property: "message", example: "Access Denied")
+            ]
+        )
+    )]
+    #[OA\Response(
+        response: 404,
+        description: "Not Found",
+        content: new OA\JsonContent(
+            type: "object",
+            properties: [
+                new OA\Property(property: "code", example: 404),
+                new OA\Property(property: "message", example: "No Type was found")
+            ]
+        )
+    )]
     public function delete(Type $type = null, TypeRepository $typeRepository): JsonResponse
     {
         // Return status code 404 if $Type is empty
--- app/backend/src/Entity/Type.php
+++ app/backend/src/Entity/Type.php
@@ -6,6 +6,7 @@ use App\Repository\TypeRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: TypeRepository::class)]
@@ -14,10 +15,12 @@ class Type
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups('read:Type:item')]
     private ?int $id = null;
 
     #[ORM\Column(length: 64)]
     #[Assert\NotBlank]
+    #[Groups('read:Type:item')]
     private ?string $name = null;
 
     #[ORM\Column]
