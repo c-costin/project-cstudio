34b2f92e36b6acad8da029db854b1909e26a5edc
c-costin <contact@costincadeau.fr>
2023-09-01 16:55:57 +0200

    feat: adding LikeService

--- app/backend/src/Repository/CategoryRepository.php
+++ app/backend/src/Repository/CategoryRepository.php
@@ -7,7 +7,7 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
- * @extends ServiceEntityRepository<Category>
+ * @extends ServiceEntityRepository<Category>œ
  *
  * @method Category|null find($id, $lockMode = null, $lockVersion = null)
  * @method Category|null findOneBy(array $criteria, array $orderBy = null)
--- app/backend/src/Repository/ProductRepository.php
+++ app/backend/src/Repository/ProductRepository.php
@@ -21,6 +21,36 @@ class ProductRepository extends ServiceEntityRepository
         parent::__construct($registry, Product::class);
     }
 
+    public function findAllLikeByUserId(int $id): ?array
+    {
+        $connexion = $this->getEntityManager()->getConnection();
+
+        $sql  = "
+            SELECT * 
+            FROM product 
+            LEFT JOIN product_user ON product.id = product_user.product_id 
+            WHERE product_user.user_id = :id;
+        ";
+
+        $result = $connexion->executeQuery($sql, ["id" => $id]);
+
+        return $result->fetchAllAssociative();
+    }
+
+    public function findLike(int $product, int $user): ?object
+    {
+        $connexion = $this->getEntityManager()->getConnection();
+
+        $sql  = "
+            SELECT *
+            FROM product
+            LEFT JOIN product_user ON product.id = product_user.product_id
+            WHERE product_user.product_id = :product and product_user.user_id = :user;
+        ";
+
+        return $connexion->executeQuery($sql, ["product" => $product, "user" => $user]);
+    }
+
 //    /**
 //     * @return Product[] Returns an array of Product objects
 //     */
