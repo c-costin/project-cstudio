a5eab609031cc2f06d5e5e87214843faea330225
mickaelprevost <prevmick.wd@gmail.com>
2023-09-05 16:55:11 +0200

    feat:add menu

---
+++ app/frontend/src/components/Menu.svelte
@@ -0,0 +1,98 @@
+<script>
+	import '$lib/styles/app.scss';
+</script>
+
+<header class="header">
+    <div class="header__left">
+        <nav class="menu hidden">
+            <i class="iconoir-cancel menu__icon js-icon-close"></i>
+            <div class="menu__links">
+                <a href="../index.html" class="menu__link">Accueil</a>
+                <a href="./about.html" class="menu__link">A Propos</a>
+            </div>
+            <div class="menu__refs">
+                <a href="" class="menu__ref">Mentions Légales</a>
+                <a href="" class="menu__ref">CGV</a>
+                <a href="" class="menu__ref">Contact</a>
+            </div>
+        </nav>
+        <i class="iconoir-menu header__icon js-icon-open"></i>
+        <i class="iconoir-search header__icon"></i>
+    </div>
+    <a href="../index.html" class="header__title">C-Studio</a>
+    <div class="header__right">
+        <i class="iconoir-profile-circle header__icon"></i>
+        <i class="iconoir-simple-cart header__icon"></i>
+    </div>
+</header> 
+
+<style lang="scss">
+.menu {
+  z-index: 1;
+  position: absolute;
+  top: 0;
+  left: 0;
+  padding: 1.5rem;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 1.75rem;
+  background: white;
+  &__icon {
+    font-size: 1.8rem;
+  }
+  &__links {
+    margin-top: 1rem;
+    display: flex;
+    flex-direction: column;
+    gap: 0.75rem;
+  }
+  &__refs {
+    display: flex;
+    flex-direction: column;
+    gap: 0.25rem;
+  }
+  &__link {
+    font-size: 2rem;
+    &:hover {
+      color: gray;
+    }
+  }
+  &__ref {
+    &:hover {
+      color: gray;
+    }
+  }
+}
+
+@media screen and (min-width: 1024px) {
+  .menu {
+    position: relative;
+    padding: 0;
+    width: fit-content;
+    display: flex!important;
+    flex-direction: row;
+    gap: 0;
+    background: none;
+    &__links {
+      margin-top: 0;
+      flex-direction: row;
+      align-items: center;
+      gap: 1.75rem;
+    }
+    &__link {
+      font-size: 1.2rem;
+      &:hover {
+        color: gray;
+      }
+    }
+    &__icon {
+      display: none;
+    }
+    &__ref {
+      display: none;
+    }
+  }
+}
+</style>
--- app/frontend/src/routes/+page.svelte
+++ app/frontend/src/routes/+page.svelte
@@ -1,11 +1,13 @@
 <script>
     import '$lib/styles/app.scss';
     import Footer from "../components/Footer.svelte";
+	import Menu from '../components/Menu.svelte';
 </script>
 
 <svelte:head>
     <title>Accueil</title>
 </svelte:head>
 
+<Menu />
 <Footer />
 
