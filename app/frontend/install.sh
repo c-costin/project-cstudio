ad16f9f51f3cb43b555e4f679ddd14a86c1eda43
c-costin <contact@costincadeau.fr>
2023-09-05 14:54:48 +0200

    feat: script install, install and basic config SvelteKit

--- app/frontend/install.sh
+++ app/frontend/install.sh
@@ -1,4 +1,10 @@
-# React
-pnpm create react-app .
-cd c-studio
-pnpm start
+#!/bin/bash
+
+# Create sveltekit project
+pnpm create svelte@latest .
+
+# Install dependancies
+pnpm install
+
+# Install SASS to DEV
+pnpm add -D sass
