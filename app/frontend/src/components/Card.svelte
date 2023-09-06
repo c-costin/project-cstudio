f1bea26cdceb9e7c8028ef3af363dc07f93ebb70
c-costin <contact@costincadeau.fr>
2023-09-06 14:44:24 +0200

    feat: create home page

---
+++ app/frontend/src/components/Card.svelte
@@ -0,0 +1,42 @@
+<script>
+	import '$lib/styles/app.scss';
+	export let isBig = false;
+</script>
+
+<a href="/" class="product-link {isBig ? 'cart-is-big' : ''}">
+	<article class="card">
+		<!-- svelte-ignore a11y-img-redundant-alt -->
+		<img class="card__img" src="/" alt="image" />
+		<div class="card__content">
+			<h3 class="card__title">Produit</h3>
+			<p class="card__price">2450€</p>
+		</div>
+	</article>
+</a>
+
+<style lang="scss">
+	.card {
+		padding: 0.25rem 0.5rem;
+		width: 100%;
+		height: fit-content;
+		&__img {
+			width: 100%;
+			aspect-ratio: 5/5;
+			border-radius: 0.5rem;
+			border: 2px solid transparent;
+			background: lightslategray;
+		}
+		&__content {
+			padding: 0.5rem 0.25rem;
+			display: flex;
+			justify-content: space-between;
+		}
+		&:hover &__img {
+			border: 2px solid black;
+		}
+	}
+	.cart-is-big {
+		grid-column: span 2;
+		grid-row: span 2;
+	}
+</style>
--- app/frontend/src/routes/+page.svelte
+++ app/frontend/src/routes/+page.svelte
@@ -1,17 +1,89 @@
 <script>
-    import '$lib/styles/app.scss';
+	import '$lib/styles/app.scss';
 	import Header from '../components/Header.svelte';
-    import Footer from "../components/Footer.svelte";
+	import Footer from '../components/Footer.svelte';
+	import Card from '../components/Card.svelte';
+	import Hero from '../components/Hero.svelte';
+
+	let products = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 </script>
 
 <svelte:head>
-    <title>C-Studio - Accueil</title>
+	<title>C-Studio - Accueil</title>
 </svelte:head>
 
 <div class="wrapper">
-    <Header />
-    <main style="height: 160vh;"></main>
+	<Header />
+
+	<Hero />
+
+	<main class="main">
+		<ul class="filter">
+			<h2 class="filter__title">C-Studio catégories</h2>
+			<li class="filter__item"><button class="filter__btn">catégorie 1</button></li>
+			<li class="filter__item"><button class="filter__btn">catégorie 2</button></li>
+			<li class="filter__item"><button class="filter__btn">catégorie 3</button></li>
+			<li class="filter__item"><button class="filter__btn">catégorie 4</button></li>
+			<li class="filter__item"><button class="filter__btn">catégorie 5</button></li>
+			<li class="filter__item"><button class="filter__btn">catégorie 6</button></li>
+		</ul>
+
+		<section class="list-products">
+			{#each products as product, i}
+				{#if i === 6 || i === 7}
+					<Card isBig={true} />
+				{:else}
+					<Card />
+				{/if}
+			{/each}
+		</section>
+	</main>
 </div>
 
 <Footer />
 
+<style lang="scss">
+	.filter {
+		margin-inline: auto;
+		padding-top: 2rem;
+		padding-bottom: 2rem;
+		padding-inline: 2rem;
+		max-width: 150%;
+		height: fit-content;
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: center;
+		gap: 0.25rem;
+		&__title {
+			width: 100%;
+			text-align: center;
+		}
+		&__item {
+			list-style-type: none;
+		}
+		&__btn {
+			border-radius: 1.5rem;
+			box-sizing: border-box;
+			display: inline-block;
+			font-size: 1rem;
+			font-weight: 600;
+			line-height: 1;
+			padding: 0.5rem 0.6rem;
+			text-align: center;
+			&:hover {
+				background-color: #1e293b;
+				color: #fff;
+			}
+		}
+	}
+	.list-products {
+		display: grid;
+		grid-template-columns: repeat(2, 1fr);
+		gap: 1rem;
+	}
+	@media screen and (min-width: 640px) {
+		.list-products {
+			grid-template-columns: repeat(4, 1fr);
+		}
+	}
+</style>
