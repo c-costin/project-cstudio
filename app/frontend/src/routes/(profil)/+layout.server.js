c28fa5cfbf98d231bc41e6e0632ebeb483e5c377
charlotte-carpentier <carpentier.charlotte@live.fr>
2023-09-29 16:39:05 +0200

    feat: Creation of group profil and layout for Profil page

--- app/frontend/src/routes/(profil)/+layout.server.js
+++ app/frontend/src/routes/(profil)/+layout.server.js
@@ -1,12 +1,13 @@
 // @ts-nocheck
 import { findAllLikeByUserId, findOrdersByUserId } from '$lib/js/request';
 
-/** @type {import('./$types').PageServerLoad} */
-export async function load({ locals }) {
+/** @type {import('./$types').LayoutServerLoad} */
+export async function load({ locals, url }) {
 	const userSession = locals.session.data.user;
 
 	return {
 		user: userSession,
+		pathname: url.pathname,
 		orders: await findOrdersByUserId(userSession.id, locals.session.data.token), 
 		likes: await findAllLikeByUserId(userSession.id, locals.session.data.token)
 	};
