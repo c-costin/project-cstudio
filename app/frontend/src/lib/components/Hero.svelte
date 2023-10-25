b651f2a056eb09aef50eb20660ca8899d39d11f2
mickaelprevost <prevmick.wd@gmail.com>
2023-10-25 19:07:18 +0200

    slider v1

--- app/frontend/src/lib/components/Hero.svelte
+++ app/frontend/src/lib/components/Hero.svelte
@@ -9,7 +9,7 @@
 	import Autoplay from 'embla-carousel-autoplay';
 
 	const autoplayOptions = {
-		delay: 4500,
+		delay: 3000,
 		stopOnInteraction: false,
 		stopOnMouseEnter: true,
 		rootNode: (emblaRoot) => emblaRoot.parentElement
@@ -25,29 +25,29 @@
 			<div class="embla__slide">
 				<img class="embla__img" src="/img/slider/sculpture.jpg" alt="sculpture" />
 				<div class="embla__content">
-					<h3 class="embla__title">Explorer les sculptures !</h3>
-					<button class="embla__btn">voir</button>
+					<h3 class="embla__title">Sculptures</h3>
+					<button class=embla__btn> Explorer</button>
 				</div>
 			</div>
 			<div class="embla__slide">
 				<img class="embla__img" src="/img/slider/dessin.webp" alt="dessin" />
 				<div class="embla__content">
-					<h3 class="embla__title">Explorer les dessins !</h3>
-					<button class="embla__btn">voir</button>
+					<h3 class="embla__title">Dessins</h3>
+					<button class=embla__btn> Explorer</button>
 				</div>
 			</div>
 			<div class="embla__slide">
 				<img class="embla__img" src="/img/slider/photo.jpg" alt="photographie" />
 				<div class="embla__content">
-					<h3 class="embla__title">Explorer les photographies !</h3>
-					<button class="embla__btn">voir</button>
+					<h3 class="embla__title">Photographies</h3>
+					<button class=embla__btn> Explorer</button>
 				</div>
 			</div>
 			<div class="embla__slide">
 				<img class="embla__img" src="/img/slider/tableau.jpg" alt="tableau" />
 				<div class="embla__content">
-					<h3 class="embla__title">Explorer les tableaux !</h3>
-					<button class="embla__btn">voir</button>
+					<h3 class="embla__title">Tableaux</h3>
+					<button class=embla__btn> Explorer</button>
 				</div>
 			</div>
 		</div>
@@ -70,17 +70,18 @@
 
 	.embla {
 		margin-inline: auto;
-		max-width: 1560px;
+		max-width: 90%;
 		overflow: hidden;
 		&__container {
-			padding: 2rem 4rem;
+			padding: 1rem 1rem;
 			display: flex;
 		}
 		&__slide {
 			margin-inline: 3rem;
 			flex: 0 0 100%;
 			display: flex;
-			justify-content: space-between;
+			justify-content: flex-start;
+			flex-direction: column;
 			align-items: center;
 			min-width: 0;
 			cursor: grab;
@@ -89,18 +90,26 @@
 			}
 		}
 		&__img {
-			width: 440px;
-			height: 296px;
+			width: 100vmin;
+			height: 30vmin;
+			border: 1px black solid;
 			object-fit: cover;
+			box-shadow: 8px 8px 10px 0 rgba(0,0,0,0.5);
+			filter:grayscale(100%);
+			-webkit-transition: all 2s;
+			&:hover {
+				filter:grayscale(0);
+				transform:scale(1.1);
+			}
 		}
 		&__content {
-			margin-right: 4rem;
 			display: flex;
-			flex-direction: column;
-			gap: 2rem;
-			align-items: flex-end;
+			flex-direction: row;
+			gap: 1rem;
+			align-items: end;
 		}
 		&__title {
+			margin-top: 1rem;
 			font-size: 2rem;
 		}
 		&__btn {
@@ -113,6 +122,15 @@
 			&:hover {
 				color: $color-white;
 			}
+		}
+	}
+
+	@media screen and (max-width: 420px) {
+		.embla {
+			&__title {
+			margin-top: 1rem;
+			font-size: 1.4rem;
+		}
 		}
 	}
 </style>
