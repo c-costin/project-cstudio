70461b841064109a265de49eac98d9385348507e
c-costin <contact@costincadeau.fr>
2023-11-04 14:27:53 +0100

    update(script)

--- app/frontend/web-server.sh
+++ app/frontend/web-server.sh
@@ -1,1 +1,1 @@
-PROTOCOL_HEADER=x-forwarded-proto HOST_HEADER=x-forwarded-host HOST=0.0.0.0 PORT=61201 node build
+PROTOCOL_HEADER=x-forwarded-proto HOST_HEADER=x-forwarded-host PORT=61201  ORIGIN=https://cstudio.costincadeau.fr node build
