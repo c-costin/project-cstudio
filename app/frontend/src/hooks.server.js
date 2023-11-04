45e013cc3d35569972805e6b912e349d2fc9ae47
c-costin <contact@costincadeau.fr>
2023-11-04 11:17:56 +0100

    upgrade(cors): change settings cors

--- app/frontend/src/hooks.server.js
+++ app/frontend/src/hooks.server.js
@@ -29,6 +29,27 @@ export const handle = handleSession(
 		]
 	},
 	({ event, resolve }) => {
+
+		// Apply CORS header for API routes
+		if (event.url.pathname.startsWith('/api')) {
+			// Required for CORS to work
+			if(event.request.method === 'OPTIONS') {
+			  	return new Response(null, {
+					headers: {
+						'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+						'Access-Control-Allow-Origin': '*',
+						'Access-Control-Allow-Headers': '*',
+					}
+				});
+			}
+		}
+		
+		const response = await resolve(event);
+
+		if (event.url.pathname.startsWith('/api')) {
+			response.headers.append('Access-Control-Allow-Origin', `*`);
+		}
+
 		const isConnected = event.locals.session.data.user;
 
 		if (event.url.pathname.startsWith('/profil')) {
